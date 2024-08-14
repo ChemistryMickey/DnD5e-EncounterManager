7af094f801ae020047a1190965b841d6ac854377
Michael Ryan T Da Silva <michele.tdasilva@gmail.com>
2024-08-13 17:14:42 -0700

    Added user database location to help text box

--- src/UI/Popups/Help/help.gd
+++ src/UI/Popups/Help/help.gd
@@ -1,7 +1,8 @@
 extends Window
 
 func _ready():
-	$TabContainer/Base.text += "\n\n Version: %s" % HardParams.VERSION
+	$TabContainer/Base.text += "\n\nVersion: %s" % HardParams.VERSION
+	$TabContainer/Base.text += "\nAll user custom databases can be found in the following location: \n\t%s" % OS.get_user_data_dir()
 
 func _on_close_requested():
 	self.move_to_center()
